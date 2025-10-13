e4b1abfe818b962a71f72cb3f635d3998205ade5
Rasmus Buurman <rasmus@rbuurman.de>
2025-10-13 02:51:33 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "sheetstorm - a typst template for university exercise sheets";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            typst
+            tinymist
+            typship
+          ];
+        };
+      }
+    );
+}
