20296e93f53ef021c7ac6be777a3253ff202087b
Rasmus Buurman <rasmus@rbuurman.de>
2025-11-14 18:58:43 +0100

    Add common.sh script

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
+source "$(dirname "$0")/common.sh"
+
 #
 # See <https://github.com/cetz-package/common/blob/main/scripts/package>
 #
@@ -15,42 +17,6 @@ files=(
 	LICENSE
 	thumbnail.png
 )
-
-function read-toml() {
-	local file="$1"
-	local key="$2"
-	# Read a key value pair in the format: <key> = "<value>"
-	# stripping surrounding quotes.
-	perl -lne "print \"\$1\" if /^${key}\\s*=\\s*\"(.*)\"/" < "$file"
-}
-
-PKG_ROOT="${PKG_ROOT:-${PWD}}"
-if [[ ! -f "$PKG_ROOT/typst.toml" ]]; then
-	echo "This script must be executed from the package root!"
-	echo "Alternatively set PKG_ROOT."
-	exit 1
-fi
-
-PKG_NAME="$(read-toml "$PKG_ROOT/typst.toml" "name")"
-if [[ -z "$PKG_NAME" ]]; then
-	echo "Could not read 'name' from $PKG_ROOT/typst.toml!"
-	exit 1
-fi
-
-PKG_VERSION="$(read-toml "$PKG_ROOT/typst.toml" "version")"
-if [[ -z "$PKG_VERSION" ]]; then
-	echo "Could not read 'version' from $PKG_ROOT/typst.toml!"
-	exit 1
-fi
-
-if [[ "$OSTYPE" == "linux"* ]]; then
-	DATA_DIR="${XDG_DATA_DIR:-$HOME/.local/share}"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	DATA_DIR="$HOME/Library/Application Support"
-else
-	DATA_DIR="${APPDATA}"
-fi
-DATA_DIR="${TYPST_PACKAGE_PATH:-${DATA_DIR}/typst/packages}"
 
 function usage() {
 	echo "package.sh TARGET [--version VERSION]"
@@ -72,7 +38,7 @@ fi
 TARGET="${1:?Missing target path or @local}"; shift
 
 if [[ "$TARGET" == "@local" ]] || [[ "$TARGET" == "local" ]]; then
-	TARGET="${DATA_DIR}/local"
+	TARGET="${TYPST_PACKAGE_PATH}/local"
 fi
 
 while [[ $# -gt 0 ]]; do
--- scripts/update-example-tests.sh
+++ scripts/update-example-tests.sh
@@ -1,32 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-function read-toml() {
-	local file="$1"
-	local key="$2"
-	# Read a key value pair in the format: <key> = "<value>"
-	# stripping surrounding quotes.
-	perl -lne "print \"\$1\" if /^${key}\\s*=\\s*\"(.*)\"/" < "$file"
-}
-
-PKG_ROOT="${PKG_ROOT:-${PWD}}"
-if [[ ! -f "$PKG_ROOT/typst.toml" ]]; then
-	echo "This script must be executed from the package root!"
-	echo "Alternatively set PKG_ROOT."
-	exit 1
-fi
-
-PKG_NAME="$(read-toml "$PKG_ROOT/typst.toml" "name")"
-if [[ -z "$PKG_NAME" ]]; then
-	echo "Could not read 'name' from $PKG_ROOT/typst.toml!"
-	exit 1
-fi
-
-PKG_VERSION="$(read-toml "$PKG_ROOT/typst.toml" "version")"
-if [[ -z "$PKG_VERSION" ]]; then
-	echo "Could not read 'version' from $PKG_ROOT/typst.toml!"
-	exit 1
-fi
+source "$(dirname "$0")/common.sh"
 
 for f in $(find "$PKG_ROOT"/examples -name '*.typ' -type f); do
 	real_example_dir="$(realpath "$(dirname "$f")")"
